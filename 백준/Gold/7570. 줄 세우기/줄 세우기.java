613463f269ece0f95807dec3daa369a92d930d36
seungtoctoc <102455571+seungtoctoc@users.noreply.github.com>
2024-01-22 14:10:49 +0900

    [Gold III] Title: 줄 세우기, Time: 460 ms, Memory: 91584 KB -BaekjoonHub

--- 백준/Gold/7570. 줄 세우기/줄 세우기.java
+++ 백준/Gold/7570. 줄 세우기/줄 세우기.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,10 +8,8 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-
-        ArrayList<Integer> kids = new ArrayList<>();
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int[] continuous = new int[n + 1];
         int max = 0;
         for (int i = 0 ; i < n ; i++) {
