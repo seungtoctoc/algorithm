982490f99ede406d8682515f6e3363b33cfd3142
seungtoctoc <102455571+seungtoctoc@users.noreply.github.com>
2024-01-24 20:14:16 +0900

    [Silver II] Title: KCPC, Time: 1172 ms, Memory: 184316 KB -BaekjoonHub

--- 백준/Silver/3758. KCPC/KCPC.java
+++ 백준/Silver/3758. KCPC/KCPC.java
@@ -1,8 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
